797761ff8a1fbe3caecc2cde6646169b8e40ae6f
antoniobenavidesvallejo <165803194+antoniobenavidesvallejo@users.noreply.github.com>
2025-02-27 10:33:28 -0500

    dashboard

--- app/query/route.ts
+++ app/query/route.ts
@@ -13,13 +13,30 @@ async function listInvoices() {
 	return data;
 }
 
+async function listRevenue() {
+	const data = await sql`SELECT * FROM revenue`;
+  return data;
+}
+
+async function lisLatestInvoices() {
+	const data = await sql`SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      ORDER BY invoices.date DESC
+      LIMIT 5`;
+  return data;
+}
+
+
 export async function GET() {
   // return Response.json({
   //   message:
   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
   // });
   try {
-  	return Response.json(await listInvoices());
+  	// return Response.json(await listInvoices());
+    // return Response.json(await listRevenue());
+    return Response.json(await lisLatestInvoices());
   } catch (error) {
   	return Response.json({ error }, { status: 500 });
   }
