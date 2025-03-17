a8547e506c06ecf20f556262a5914ce7018ea19a
antoniobenavidesvallejo <165803194+antoniobenavidesvallejo@users.noreply.github.com>
2025-03-17 12:18:49 -0500

    error deploy

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -82,7 +82,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     } catch (error){
     // If a database error occurs, return a more specific error.
         return {
-            message: 'Database Error: Failed to Create Invoice.'
+            message: 'Database Error: Failed to Create Invoice. ' + error
         };
     }
 
@@ -118,7 +118,7 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
     } catch (error) {
-      return { message: 'Database Error: Failed to Update Invoice.' };
+      return { message: 'Database Error: Failed to Update Invoice. ' + error };
     }
    
     revalidatePath('/dashboard/invoices');
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -66,9 +66,18 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+            ))}
           </div>
         </div>
 
