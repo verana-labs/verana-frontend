1e043e326bf5d692baf1a1cf60381e49df68422c
Maxime Mansiet <airkyzzz.jiji@gmail.com>
2026-03-16 20:37:22 +0100

    feat: Add dashboard link to logo in navbar (#329)
    * feat: Add dashboard link to logo in navbar
    Closes #324
    
    * fix: Add alt text for Veranito Logo in English and Spanish translations

--- app/ui/common/verana-logo.tsx
+++ app/ui/common/verana-logo.tsx
@@ -1,11 +1,15 @@
 'use client';
 
+import Link from "next/link";
 import { configFooter } from "@/lib/dashlinks";
+import { translate } from "@/i18n/dataview";
 
 export default function VeranaLogo() {
   return (
     <div className="logo-container">
-      <img src={configFooter.img} alt="Veranito Logo" className="w-8 h-8"/>
+      <Link href={configFooter.href}>
+        <img src={configFooter.img} alt={translate("navbar.logo.alt")} className="w-8 h-8"/>
+      </Link>
       <h1 className="logo-label">{configFooter.title}</h1>
     </div>
   );
