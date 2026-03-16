51ad568720b97fa682d9b2058da00e7da61cd8f3
AirKyzzZ <airkyzzz.jiji@gmail.com>
2026-03-16 16:35:10 +0100

    fix: Add alt text for Veranito Logo in English and Spanish translations

--- app/ui/common/verana-logo.tsx
+++ app/ui/common/verana-logo.tsx
@@ -2,12 +2,13 @@
 
 import Link from "next/link";
 import { configFooter } from "@/lib/dashlinks";
+import { translate } from "@/i18n/dataview";
 
 export default function VeranaLogo() {
   return (
     <div className="logo-container">
       <Link href={configFooter.href}>
-        <img src={configFooter.img} alt="Veranito Logo" className="w-8 h-8"/>
+        <img src={configFooter.img} alt={translate("navbar.logo.alt")} className="w-8 h-8"/>
       </Link>
       <h1 className="logo-label">{configFooter.title}</h1>
     </div>
